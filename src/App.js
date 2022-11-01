b0a7d04b02d308c975eb753f4c2ca8b5477cd63b
Suvam Gupta <suvamgupta131999@gmail.com>
2022-11-02 00:17:10 +0530

    Added UserList Component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import Sidebar from "./components/sidebar/Sidebar";
 import Topbar from "./components/topbar/Topbar";
 import "./app.css";
-import Home from "./pages/Home";
+import Home from "./pages/home/Home";
+import UserList from "./pages/userList/UserList";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
 
 function App() {
   return (
@@ -9,7 +16,12 @@ function App() {
       <Topbar />
       <div className="container">
         <Sidebar />
-        <Home />
+        <Router>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/users" element={<UserList />} />
+          </Routes>
+        </Router>
       </div>
     </div>
   );
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,9 +1,9 @@
-import Chart from "../components/chart/Chart";
-import FeaturedInfo from "../components/featuredInfo/FeaturedInfo";
+import Chart from "../../components/chart/Chart";
+import FeaturedInfo from "../../components/featuredInfo/FeaturedInfo";
 import "./home.css";
-import { userData } from "../dummyData";
-import WidgetLg from "../components/widgetLg/WidgetLg";
-import WidgetSm from "../components/widgetSm/WidgetSm";
+import { userData } from "../../dummyData";
+import WidgetLg from "../../components/widgetLg/WidgetLg";
+import WidgetSm from "../../components/widgetSm/WidgetSm";
 
 export default function Home() {
   return (
